2f4e0094e61626f0f9414a669c76a0b82154fb9d
bagus.yanuar613 <bagus.yanuar613@gmail.com>
2022-04-07 12:41:57 +0700

    login
    last kotlin version 1.3.50

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -8,6 +10,11 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String username = '';
+  String password = '';
+  bool isLoading = false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,14 +22,77 @@ class _LoginState extends State<Login> {
         child: Container(
           height: MediaQuery.of(context).size.height,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Center(
                 child: Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
+                  height: 65,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  margin: EdgeInsets.only(bottom: 20),
                   decoration: BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage("assets/logo.png"), fit: BoxFit.fill),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                margin: EdgeInsets.only(bottom: 15),
+                child: TextField(
+                  onChanged: (text){
+                    setState(() {
+                      username = text;
+                    });
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      prefixIcon: Icon(Icons.account_circle_outlined),
+                      hintText: "Username"),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                margin: EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  onChanged: (text){
+                    setState(() {
+                      password = text;
+                    });
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      prefixIcon: Icon(Icons.lock_outline),
+                      hintText: "Password"),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: GestureDetector(
+                  onTap: () {
+                    login();
+                  },
+                  child: Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red[400]
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               )
@@ -31,5 +101,32 @@ class _LoginState extends State<Login> {
         ),
       ),
     );
+  }
+
+
+  void login() async {
+      print(username);
+      print(password);
+      Map<String, String> data = {
+        "username": username,
+        "password": password
+      };
+      var formData = FormData.fromMap(data);
+      final response = await Dio().post(
+          "localhost:8000/api/login",
+          options: Options(
+            headers: {
+              "accept": "application/json"
+            },
+          ),
+          data: formData
+      );
+      print(response.data);
+      final int code = response.data["status"] as int;
+      if (code == 200) {
+        final String token = response.data["msg"] as String;
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        preferences.setString("token", token);
+      }
   }
 }
